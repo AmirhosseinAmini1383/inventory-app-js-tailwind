8595e5e830e029d796d3d787fd83d9d5a93c0fbb
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-09-16 18:13:52 +0330

    coding CategoryView setApp - createCategoriesList functions

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -1,18 +1,37 @@
 import Storage from "./Storage.js";
-const title = document.querySelector("#category-title");
-const description = document.querySelector("#category-description");
+const categoryTitle = document.querySelector("#category-title");
+const categoryDescription = document.querySelector("#category-description");
 const addNewCategoryBtn = document.querySelector("#add-new-category");
-export default class CategoryView {
+class CategoryView {
   constructor() {
     addNewCategoryBtn.addEventListener("click", (e) => this.addNewCategory(e));
     this.Categories = [];
   }
   addNewCategory(e) {
     e.preventDefault();
-    const title = title.value;
-    const description = description.value;
+    const title = categoryTitle.value;
+    const description = categoryDescription.value;
     if (!title || !description) return;
     Storage.saveCategory({ title, description });
     this.Categories = Storage.getAllCategories();
+    this.createCategoriesList();
+    categoryTitle.value = "";
+    categoryDescription.value = "";
+  }
+  setApp() {
+    this.Categories = Storage.getAllCategories();
+  }
+  createCategoriesList() {
+    let result = `<option class="bg-slate-500 text-slate-300" value="">select a category</option>;`;
+    this.Categories.forEach((item) => {
+      result += `
+        <option class="bg-slate-500 text-slate-300" value=${item.id}>
+            ${item.title}        
+        </option>;
+        `;
+    });
+    const categoriesOptions = document.querySelector("#product-category");
+    categoriesOptions.innerHTML = result;
   }
 }
+export default new CategoryView();
