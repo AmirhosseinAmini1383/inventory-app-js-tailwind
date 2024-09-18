1ed5685a3207a6e94ca4197e2862812c89285420
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-09-18 16:37:10 +0330

    coding Number of products (length) on the appbar

--- src/js/ProductView.js
+++ src/js/ProductView.js
@@ -6,6 +6,7 @@ const productCategory = document.querySelector("#product-category");
 const addNewProductBtn = document.querySelector("#add-new-product");
 const searchInput = document.querySelector("#search-input");
 const selectedSort = document.querySelector("#sort-products");
+const productsLenght = document.querySelector("#product-lenght");
 class ProductView {
   constructor() {
     addNewProductBtn.addEventListener("click", (e) => this.addNewProduct(e));
@@ -70,6 +71,8 @@ class ProductView {
     deleteBtns.forEach((btn) => {
       btn.addEventListener("click", (e) => this.deleteProducts(e));
     });
+
+    productsLenght.innerText = products.length;
   }
   searchProducts(e) {
     const value = e.target.value.trim().toLowerCase();
